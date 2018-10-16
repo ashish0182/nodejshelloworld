0dd4620f051e6656212b8d348c266717b9f7b00f
ashish sharma <ashish11.sharma@paytm.com>
2018-10-16 23:24:24 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var process = require("process");
+
+console.log("hello world");
+var a = "ashish";
+var b = "sharma";
+var result = a + " " + b ;
+console.log(result);
+var p = 100;
+var r = 50;
+var t = 100;
+var result = (p*r*t)/100;
+console.log("simple interest = " + result);
+
+var stdin = process.openStdin();
+stdin.addListener("data",function(ptrstring){
+console.log(ptrstring.toString());
+
+var ptrarray = ptrstring.toString().trim().split(" ");
+var arr = [];
+for(var i=0;i<ptrarray.length;i++)
+{
+var gtr = Number(ptrarray[i]);
+arr.push(gtr);
+//console.log(gtr);
+}
+
+console.log(arr);
+})
